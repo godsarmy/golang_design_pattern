87952112bdb8f6e7a4010870e1eda4a22da64321
godsarmy <godsarmycy@gmail.com>
2012-12-02 12:32:32 +0800

    Add iterator mode

---
+++ src/iterator.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+//original iterator
+type Iterator struct {
+    current  int
+    data_set *DataSet
+}
+
+func (p *Iterator) has_next() bool {
+    length := len(*p.data_set)
+    rc := true
+    if p.current >= length {
+        rc = false
+    }
+    return rc
+}
+
+type StringIterator struct {
+    Iterator
+}
+
+type DataSet []string
+
+func (p *DataSet) get_iterator() *StringIterator {
+    iter := new(StringIterator)
+    iter.current = 0
+    iter.data_set = p
+    return iter
+}
+
+//specific return function
+func (p *StringIterator) next() string {
+    data := (*p.data_set)[p.current]
+    p.current++
+    return data
+}
+
+func main() {
+    data := &DataSet{"foo", "bar", "hello", "world"}
+
+    for iter := data.get_iterator(); iter.has_next(); {
+        value := iter.next()
+        fmt.Println("Value: ", value)
+    }
+}
