141e80b3bac5093e46d2a81811b38f200a57d8d5
godsarmy <godsarmycy@gmail.com>
2012-12-08 21:10:25 +0800

    add mediator pattern

---
+++ src/mediator.go
@@ -0,0 +1,59 @@
+//golang version of java code in http://en.wikipedia.org/wiki/Strategy_pattern
+package main
+
+import "fmt"
+
+//Mediator
+type Mediator interface {
+    send(message string, colleague Colleague)
+}
+
+type ConcreteMediator struct {
+    colleague1 *ConcreteColleague1
+    colleague2 *ConcreteColleague2
+}
+
+func (p *ConcreteMediator) send(message string, colleague Colleague) {
+    if colleague == p.colleague1 {
+        p.colleague2.notify(message)
+    } else {
+        p.colleague1.notify(message)
+    }
+}
+
+type Colleague interface{}
+
+//Colleague
+type ConcreteColleague1 struct {
+    mediator Mediator
+}
+
+func (p *ConcreteColleague1) send(message string) {
+    p.mediator.send(message, p)
+}
+
+func (p *ConcreteColleague1) notify(message string) {
+    fmt.Println("Colleague1 gets message:", message)
+}
+
+type ConcreteColleague2 struct {
+    mediator Mediator
+}
+
+func (p *ConcreteColleague2) send(message string) {
+    p.mediator.send(message, p)
+}
+
+func (p *ConcreteColleague2) notify(message string) {
+    fmt.Println("Colleague2 gets message:", message)
+}
+
+func main() {
+    m := &ConcreteMediator{}
+    c1 := &ConcreteColleague1{m}
+    c2 := &ConcreteColleague2{m}
+    m.colleague1 = c1
+    m.colleague2 = c2
+    c1.send("How are you?")
+    c2.send("Fine, thanks")
+}
